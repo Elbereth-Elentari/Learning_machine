3192e4cd7df9aa54d27e2b7b285f7780653fb5fe
Helena Sobol <arvala@wp.pl>
2019-11-24 15:50:28 +0100

    Renamed to flashcards. More questions.

--- learning_machine.py
+++ learning_machine.py
@@ -12,18 +12,18 @@ print("Last level:", last_level)
 
 session_count = 0
 current_level = int(last_level) # Current_level is an integer
-with open("fiszki.csv", "r") as fiszki:
-    fiszki = pd.read_csv(fiszki, header=None)
+with open("flashcards.csv", "r") as flashcards:
+    flashcards = pd.read_csv(flashcards, header=None)
 
 while session_count < session_limit:
 
     # Check if there are questions at the current level. This is iterative, because we want to exhaust the current level.
-    if len(fiszki.loc[fiszki[2] == int(current_level)]) == 0:
+    if len(flashcards.loc[flashcards[2] == int(current_level)]) == 0:
         print("NO MORE QUESTIONS AT LEVEL", current_level)
         # Find another level, save it as integer
         x = 0
         while x in range(0, 5):
-            if len(fiszki.loc[fiszki[2] == x]) > 0:
+            if len(flashcards.loc[flashcards[2] == x]) > 0:
                 current_level = x
                 break
             else:
@@ -38,15 +38,15 @@ while session_count < session_limit:
         # Check if the level directly above hasn't maxed up. If it has, we have to go to that level before continuing. The new level is saved as an integer.
         if current_level < 4:
             next_level = current_level + 1
-            if len(fiszki.loc[fiszki[2] == next_level]) >= max_definitions[int(next_level)]:
+            if len(flashcards.loc[flashcards[2] == next_level]) >= max_definitions[int(next_level)]:
                 current_level += 1
                 print("NEXT LEVEL MAXED OUT.\nNEW LEVEL:", current_level)
-        row = fiszki.loc[fiszki[2] == current_level].sample(n=1)
+        row = flashcards.loc[flashcards[2] == current_level].sample(n=1)
         # Display as many questions as possible: either before session end, or before end of questions at the level.
         print("Question:", row[0].values[0])
         answer = input("Answer:")
         if answer == row[1].values[0]:
-            fiszki.loc[fiszki[0] == row[0].values[0], 2] += 1
+            flashcards.loc[flashcards[0] == row[0].values[0], 2] += 1
             print("Correct.")
         else:
             print("Wrong.")
@@ -55,13 +55,13 @@ while session_count < session_limit:
 
 print("END OF SESSION.")
 
-with open("fiszki.csv", "w") as nowe_fiszki, open("answered_questions.csv", "a+") as answered:
-    fiszki.loc[fiszki[2] < 5].to_csv(nowe_fiszki, header=False, index=False)
-    fiszki.loc[fiszki[2] == 5].to_csv(answered, header=False, index=False)
+with open("flashcards.csv", "w") as nowe_flashcards, open("answered_questions.csv", "a+") as answered:
+    flashcards.loc[flashcards[2] < 5].to_csv(nowe_flashcards, header=False, index=False)
+    flashcards.loc[flashcards[2] == 5].to_csv(answered, header=False, index=False)
 
 if last_level != str(current_level):
     with open("last_level.txt", "w") as new:
         new.write(str(current_level))
 
 for level in range(0, 5):
-    print("Remaining questions at level", level, ":", len(fiszki.loc[fiszki[2] == level]), "/", max_definitions[level])
+    print("Remaining questions at level", level, ":", len(flashcards.loc[flashcards[2] == level]), "/", max_definitions[level])
