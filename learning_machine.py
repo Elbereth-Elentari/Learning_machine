aea34fa25b41bc2e072eb69e4124f17c9ed233d5
Helena Sobol <arvala@wp.pl>
2020-01-04 08:07:12 +0100

    Removed repetitive outputs.

--- learning_machine.py
+++ learning_machine.py
@@ -11,8 +11,12 @@ dash = "---------------------------------------------------"
 
 print(equals * 2)
 
+def questions(checked_level):
+    questions = flashcards.loc[flashcards[2] == checked_level]
+    return questions
+
 for level in range(0, 5):
-    print("Remaining questions at level", level, ":", len(flashcards.loc[flashcards[2] == level]), "/", max_definitions[level])
+    print("Remaining questions at level", level, ":", len(questions(level)), "/", max_definitions[level])
 
 with open("last_level.txt", "r") as saved_level:
     for line in saved_level:
@@ -21,63 +25,66 @@ with open("last_level.txt", "r") as saved_level:
 print("CURRENT LEVEL:", last_level)
 print(equals * 2)
 
-session_count = 0
 current_level = int(last_level)
 
-def check_for_max(current_level):
-    if len(flashcards.loc[flashcards[2] == current_level + 1]) >= max_definitions[current_level + 1]:
-        current_level += 1
-        print(equals * 2, "\nNEXT LEVEL MAXED OUT.\nNEW LEVEL:", current_level)
+session_count = 0
+
+def check_for_max(level):
+    if level < 4 and len(questions(level + 1)) >= max_definitions[level + 1]:
+        level += 1
+        print(equals * 2, "\nNEXT LEVEL MAXED OUT.\nNEW LEVEL:", level)
         print(equals * 2)
         return True
     else:
         return False
 
 while session_count < session_limit:
-    if len(flashcards.loc[flashcards[2] == int(current_level)]) == 0:
-        print( equals * 2, "\nNO MORE QUESTIONS AT LEVEL", current_level)
-        x = 0
-        while x in range(0, 5):
-            if len(flashcards.loc[flashcards[2] == x]) > 0:
-                current_level = x
+    if len(questions(level)) == 0:
+        print( equals * 2, "\nNO MORE QUESTIONS AT LEVEL", level)
+        for level in range(0, 5):
+            if len(questions(level)) > 0:
                 break
-            else:
-                x += 1
-        if x == 5:
+        if level == 5:
             print("NO MORE QUESTIONS.")
             print(equals * 2)
             break
         else:
-            print("NEW LEVEL:", current_level)
+            print("NEW LEVEL:", level)
             print(equals * 2)
 
     else:
-        if current_level < 4 and check_for_max(current_level):
-            current_level += 1
+        if check_for_max(level):
+            level += 1
 
-        level = flashcards.loc[flashcards[2] == current_level]
-        for row in range(0,len(level)):
-            if session_count < session_limit and check_for_max(current_level) == False:
-                print(dash, " QUESTION", session_count, ":", dash, "\n", level.iloc[row][0])
-                answer = input("ANSWER:")
-                if answer == level.iloc[row][1]:
-                    flashcards.loc[flashcards[0] == level.iloc[row][0], 2] += 1
-                    print("CORRECT.")
+        for row in range(0,len(questions(level))):
+            if session_count < session_limit:
+                if check_for_max(level) == False:
+                    questions_df = flashcards.loc[flashcards[2] == level]
+                    print(dash, " QUESTION", session_count, ":", dash, "\n", questions_df.iloc[row][0])
+                    answer = input("ANSWER:")
+                    if answer == questions_df.iloc[row][1]:
+                        flashcards.loc[flashcards[0] == questions_df.iloc[row][0], 2] += 1
+                        print("CORRECT.")
+                    else:
+                        print("WRONG.")
+                        print("                                                   CORRECT ANSWER:\n", questions_df.iloc[row][1])
+                    session_count += 1
                 else:
-                    print("WRONG.")
-                    print("                                                   CORRECT ANSWER:\n", level.iloc[row][1])
-                session_count += 1
+                    level += 1
+                    break
+            else:
+                break
 
 print(equals, "\nEND OF SESSION.")
 print(equals)
 
-with open("flashcards.csv", "w") as nowe_flashcards, open("answered_questions.csv", "a+") as answered:
-    flashcards.loc[flashcards[2] < 5].to_csv(nowe_flashcards, header=False, index=False, line_terminator="\n")
+with open("flashcards.csv", "w") as new_flashcards, open("answered_questions.csv", "a+") as answered:
+    flashcards.loc[flashcards[2] < 5].to_csv(new_flashcards, header=False, index=False, line_terminator="\n")
     flashcards.loc[flashcards[2] == 5].to_csv(answered, header=False, index=False, line_terminator="\n")
 
-if last_level != str(current_level):
+if last_level != str(level):
     with open("last_level.txt", "w") as new:
-        new.write(str(current_level))
+        new.write(str(level))
 
 for level in range(0, 5):
-    print("Remaining questions at level", level, ":", len(flashcards.loc[flashcards[2] == level]), "/", max_definitions[level])
+    print("Remaining questions at level", level, ":", len(questions(level)), "/", max_definitions[level])
